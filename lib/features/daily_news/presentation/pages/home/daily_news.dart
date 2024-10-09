b2853ac9587f79fd5dee1d686abcf84103636d0f
Samuel <samuelketema01@gmail.com>
2024-10-09 22:24:09 +0300

    homepage

---
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  _buildAppBar() {
+    return AppBar(
+      title: Text(
+        'Daily News',
+        style: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  _buildBody() {
+    return BlocBuilder<RemoteArticleBloc, RemoteArticleState>(
+        builder: (_, state) {
+      if (state is RemoteArticlesLoading) {
+        return const Center(
+          child: CupertinoActivityIndicator(),
+        );
+      }
+      if (state is RemoteArticlesError) {
+        return const Center(
+          child: Icon(Icons.refresh),
+        );
+      }
+      if (state is RemoteArticlesDone) {
+        return ListView.builder(
+          itemCount: state.articles!.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('$index'),
+            );
+          });
+      }
+      return const SizedBox();
+    });
+  }
+}
