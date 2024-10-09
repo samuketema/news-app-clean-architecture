1c72ded986e9fb177988a9d9bd857fb767fd0ee9
Samuel <samuelketema01@gmail.com>
2024-10-09 22:22:44 +0300

    new file for constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,4 +1,4 @@
-const String newsApiBaseUrl = 'https://newsapi.orf/v2';
+const String newsApiBaseUrl = 'https://newsapi.org/v2';
 const String newsApiKey = '76dcf6b1498e404ea2d8e1f261e39ba6';
 const String countryQuery = 'us';
-const String categoryQuery = 'general';
+const String categoryQuery = 'business';
